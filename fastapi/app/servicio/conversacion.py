6156da8e7070b5f1154bb3885e35d286a904c7ee
Juand13go <ramirezrendonjuandiego@gmail.com>
2026-07-15 20:32:23 -0500

    refactor: Actualizacion de la funcion comunicacion_agente (Mejoras en el prompt y eliminacion de un if innecesario)

--- fastapi/app/servicio/conversacion.py
+++ fastapi/app/servicio/conversacion.py
@@ -36,76 +36,66 @@ client = OpenAI(api_key=os.getenv("GROQ_API_KEY"), base_url="https://api.groq.co
 def comunicacion_agente(id_conversacion: uuid.UUID, session: Session):
     historial = obtener_historial_conversacion(id_conversacion=id_conversacion, session=session)
     catalogo_productos_variable = catalogo_a_texto(session) 
-    if not historial:  #Es mas de python usar if not historial: que es lo mismo que if historial == []:
-        return {
-            "respuesta": "¡Hola! Bienvenido a Industrias Rambler S.A. ¿Desde qué ciudad nos contactas?",
-            "escalar": False,
-            "productos_interes": "",
-            "ciudad": ""
-        }
-    else:
-        response = client.chat.completions.create(
-            model = "llama-3.3-70b-versatile",
-            max_tokens = 1024,
-            messages=[{
-                        "role": "system", "content": f"""Eres el agente encargado del manejo de cotizaciones de Industrias Rambler S.A, vas a recibir el historial de un chat con 
-                        los mensajes del cliente + el catalogo con los productos: {catalogo_productos_variable} Ten en cuenta que el publico al que te diriges es de Colombia, empieza el chat saludando al cliente y preguntandole por su ubicación 
-                        (La retornaras en ciudad, pero en caso de que no conteste con esa respuesta AÚN, a ciudad le asignas valor "" (un str vacío)), 
-                        retornar los productos de interes en caso de que el cliente los mencione si no retornas "" (un str vacío), entregar una respuesta para enviarla al chat con el cliente; entregar un valor booleano marcando el estado de escalación 
-                        del lead, marca True (Como booleano) si el cliente muestra clara intención de compra (ej. quiere dejar sus datos, pide cotización formal, pregunta dónde 
-                        pagar), si está muy enojado o si pide explicitamente hablar con un asesor humano; OBSERVACION IMPORTANTE: JAMAS ESCALES A TRUE CON LOS CAMPOS "productos_interes" o "ciudad" vacíos, NO escales todavía: tu tarea es preguntárselo de forma amable en respuesta_cliente. Solo marca debe_escalar: True cuando ya tengas ambos datos... De lo contrario, marca False (Como booleano). El mensaje de texto 
-                        amigable, comercial y profesional que el bot le enviará al usuario por Telegram respondiendo sus dudas usando el catalogo"""
-            }] + [{"role" : m.rol, "content" : m.contenido} for m in reversed(historial)],
-            tools=[
-                {
-                    "type" : "function",
-                    "function": {
+    response = client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        max_tokens = 1024,
+        messages=[{
+                    "role": "system", "content": f"""Eres el agente encargado del manejo de cotizaciones de Industrias Rambler S.A, vas a recibir el historial de un chat con los mensajes del cliente + el catalogo con los productos: {catalogo_productos_variable}. Ten en cuenta que el publico al que te diriges es de Colombia; 
+                    empieza el chat saludando al cliente y preguntandole por su ubicación;
+                    Primero consigue ciudad y productos_interes, solo cuando tengas ambos si el cliente tiene clara intención de compra (ej. quiere dejar sus datos, pide cotización formal, pregunta dónde pagar), si está muy enojado o si pide explícitamente hablar con un asesor humano actualiza el estado de escalación a True. De lo contrario, marca False (Como booleano).
+                    Si el cliente no proporciona información en ciudad o productos_interes, llena esas variables con un string vacío ("").
+                    El cliente no debe saber el estado de escalación del Lead, ni si sera conectado con un asesor."""
+        }] + [{"role" : m.rol, "content" : m.contenido} for m in reversed(historial)],
+        tools=[
+            {
+                "type" : "function",
+                "function": {
 
-                        "name" : "evaluar_y_responder",
-                        "description" : "Herramienta obligatoria para generar la respuesta al cliente de Industrias Rambler S.A y decidir si se requiere atención humana.",
+                    "name" : "evaluar_y_responder",
+                    "description" : "Herramienta obligatoria para generar la respuesta al cliente de Industrias Rambler S.A y decidir si se requiere atención humana.",
 
-                        "parameters":{
-                            "type" : "object",
-                            
-                            "properties" : {
-                                "respuesta_cliente" : {
-                                    "type" : "string",
-                                    "description" : "El mensaje de texto amigable, comercial y profesional que el bot le enviará al usuario por Telegram respondiendo sus dudas usando el catalogo"
-                                },
-                                "productos_interes" : {
-                                    "type" : ["string"],
-                                    "description" : "Retornar los productos de interes del cliente."
-                                },
-                                "ciudad" : {
-                                    "type" : "string",
-                                    "description" : "Ciudad desde donde el cliente realiza el contacto."
-                                },
-                                "debe_escalar" : {
-                                    "type" : "boolean",
-                                    "description" : "Marca True si el cliente muestra clara intención de compra. De lo contrario, marca False"
-                                }
+                    "parameters":{
+                        "type" : "object",
+                        
+                        "properties" : {
+                            "respuesta_cliente" : {
+                                "type" : "string",
+                                "description" : "El mensaje de texto amigable, comercial y profesional que el bot le enviará al usuario por Telegram respondiendo sus dudas usando el catalogo"
                             },
-                            "required" : ["respuesta_cliente", "debe_escalar"]
-                        }
+                            "productos_interes" : {
+                                "type" : ["string"],
+                                "description" : "Retornar los productos de interes del cliente."
+                            },
+                            "ciudad" : {
+                                "type" : "string",
+                                "description" : "Ciudad desde donde el cliente realiza el contacto."
+                            },
+                            "debe_escalar" : {
+                                "type" : "boolean",
+                                "description" : "Actualizar el estado de escalación (True o False)"
+                            }
+                        },
+                        "required" : ["respuesta_cliente", "debe_escalar"]
                     }
                 }
-            ],
-            tool_choice={"type": "function", "function": {"name": "evaluar_y_responder"}}
-        )
-        texto_respuesta = response.choices[0].message.tool_calls[0].function.arguments
-        datos_parseados = json.loads(texto_respuesta)
+            }
+        ],
+        tool_choice={"type": "function", "function": {"name": "evaluar_y_responder"}}
+    )
+    texto_respuesta = response.choices[0].message.tool_calls[0].function.arguments
+    datos_parseados = json.loads(texto_respuesta)
 
-        respuesta = datos_parseados.get("respuesta_cliente", "")
-        escalar = True if (datos_parseados.get("debe_escalar", False) in [True, "True", "true"]) else False
-        productos_interes = (datos_parseados.get("productos_interes") or  "").strip()
-        ciudad = (datos_parseados.get("ciudad") or "").strip()
+    respuesta = datos_parseados.get("respuesta_cliente", "")
+    escalar = True if (datos_parseados.get("debe_escalar", False) in [True, "True", "true"]) else False
+    productos_interes = (datos_parseados.get("productos_interes") or  "").strip()
+    ciudad = (datos_parseados.get("ciudad") or "").strip()
 
-        if not ciudad or not productos_interes:
-            escalar = False
+    if not ciudad or not productos_interes:
+        escalar = False
 
-        return {
-            "respuesta" : respuesta, 
-            "escalar" : escalar,
-            "productos_interes": productos_interes,
-            "ciudad": ciudad 
-        }
+    return {
+        "respuesta" : respuesta, 
+        "escalar" : escalar,
+        "productos_interes": productos_interes,
+        "ciudad": ciudad
+    }
